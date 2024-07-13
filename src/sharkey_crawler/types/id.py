aa1fa613ba8e76f7e4a934d0fd35cb091cfc9b0d
Leo <leo@hexafuchs.de>
2024-07-13 12:55:08 +0200

    :fire: Remove support for 3.9

--- src/sharkey_crawler/types/id.py
+++ src/sharkey_crawler/types/id.py
@@ -5,15 +5,9 @@
 """This defines types related to ids."""
 from __future__ import annotations
 
-from sys import version_info
-
-if version_info[0] > 3 or version_info[1] >= 10:
-    from typing import TypeAlias
+from typing import TypeAlias
 
 __all__ = ["SharkeyId"]
 
-if version_info[0] > 3 or version_info[1] >= 10:
-    """The current representation of an ID in Sharkey."""
-    SharkeyId: TypeAlias = str
-else:
-    SharkeyId = str
+"""The current representation of an ID in Sharkey."""
+SharkeyId: TypeAlias = str
