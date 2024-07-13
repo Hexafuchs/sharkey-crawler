85964f764a9840147a0de244cc6601b820846527
Leo <leo@hexafuchs.de>
2024-07-13 16:58:57 +0200

    :wrench: Mute flake8 issues as they are not relevant for this

--- src/sharkey_crawler/types/__init__.py
+++ src/sharkey_crawler/types/__init__.py
@@ -2,6 +2,7 @@
 #   Copyright (c) Hexafuchs. All rights reserved.
 #   Licensed under the MIT License. See LICENSE in project root for information.
 #   -------------------------------------------------------------
+# flake8: noqa: F403,F401
 """This provides access to all types."""
 
 from .id import *
